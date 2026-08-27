69e4e017c45936fa5ce85f6f2e93e6868206d569
Forevernewvie <cjb11055@naver.com>
2026-08-27 16:05:29 +0900

    fix(wear): install splash screen in MainActivity to prevent crashes

--- wear_app/android/app/src/main/kotlin/com/forevernewvie/projectgtg/MainActivity.kt
+++ wear_app/android/app/src/main/kotlin/com/forevernewvie/projectgtg/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.forevernewvie.projectgtg
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
+}
