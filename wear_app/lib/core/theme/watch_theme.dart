ba73cd55d7af69fc7d80859ff5ad17e472de148a
Forevernewvie <cjb11055@naver.com>
2026-08-27 16:03:00 +0900

    fix(wear): address Google Play Store rejection reasons
    - Set pure black background for OLED displays
    - Migrate to standard AndroidX Core Splash Screen API
    - Add required WAKE_LOCK permission for AOD mode
    - Add required wearable shared library declaration in AndroidManifest

--- wear_app/lib/core/theme/watch_theme.dart
+++ wear_app/lib/core/theme/watch_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class WatchTheme {
   static const Color slateGray = Color(0xFF2C2F33);
-  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkBackground = Colors.black;
   static const Color neonMint = Color(0xFF00FF9D);
 
   static ThemeData get darkTheme {
